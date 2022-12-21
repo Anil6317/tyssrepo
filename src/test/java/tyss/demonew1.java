48e3868feabe9a6cacee9249ffd17a67d57a708e
Anil6317 <anil6317@gmail.com>
2022-12-21 23:21:09 +0530

    leo

---
+++ src/test/java/tyss/demonew1.java
@@ -0,0 +1,12 @@
+package tyss;
+
+public class demonew1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("go to hell");
+		System.out.println("joooo goooo");
+
+	}
+
+}
